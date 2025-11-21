fea5e2e6bf44784566d162c2127cf24c1825d665
Aoiizu <alifahnafh3@gmail.com>
2025-11-22 00:30:25 +0900

    Update Lagi ke 3x nya

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lecturers</title>
+    <title>Admin</title>
     <link href="{{ ('assets/output.css') }}" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
